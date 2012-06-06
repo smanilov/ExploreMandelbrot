3907247e3d0d270ccde24d0956b1df7718c458a1
sm <stanislav.manilov@gmail.com>
2012-06-06 14:45:28 +0300

    .

---
+++ Mandelbrot/src/smanilov/mandelbrot/Colors.java
@@ -0,0 +1,17 @@
+package smanilov.mandelbrot;
+
+import java.awt.Color;
+
+public class Colors {
+
+	private static Color color = new Color(192, 244, 11);
+	
+	public static Color getColor() {
+		return color;
+	}
+	
+	public static void setColor(Color color) {
+		Colors.color = color; 
+	}
+
+}
